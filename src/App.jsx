e4c54543d9afb5655e6a26638ce2a75c1b4f9892
kbhavre78 <kbhavre78@gmail.com>
2025-01-10 16:51:36 +0530

    Initial setup completed (font and colors)

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+const App = () => {
+  return (
+    <div>
+      <h1 className='text-[100px] font-kyiv text-secondary font-medium  tracking-tight text-center'>B Kunal</h1>
+      <h1 className='text-[100px] font-kyiv text-secondary font-medium text-center'>B Kunal</h1>
+
+    </div>
+  )
+}
+
+export default App
---
+++ tailwind.config.js
@@ -0,0 +1,23 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+ content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      fontFamily: {
+        kyiv: ['KyivTypeSans'], // Add KyivType Sans
+      },
+      colors: {
+        primary: "#F4F2ED",
+        secondary: "#FF6542",
+        accent: "#FAEDBC",
+        myBlack: "#111214",
+        myGrey: "#C4C4C4"
+      }
+    },
+  },
+  plugins: [],
+}
+
