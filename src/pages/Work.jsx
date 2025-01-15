1b546015f1f4ca90c4c1afe2f8706c07c6987591
kbhavre78 <kbhavre78@gmail.com>
2025-01-15 19:19:56 +0530

    Fixed scrolling issue in work page

--- src/pages/Work.jsx
+++ src/pages/Work.jsx
@@ -3,19 +3,18 @@ import { projects } from "../constants"
 
 const Work = () => {
   return (
-    <div className='w-full my-8 px-4 md:px-0'>
-      {/* <h1 className="text-center text-secondary text-8xl font-kyiv font-bold">Work</h1> */}
-      <div className="grid grid-cols-1 md:grid-cols-2 gap-4">
+    <div className='w-full h-screen overflow-y-auto px-4 md:px-0 pb-16'>
+    <div className="grid grid-cols-1 md:grid-cols-2 gap-4 my-8">
       {projects.map((project) => (
         <WorkCard
-          key={project.id} // Unique key based on the id
+          key={project.id}
           title={project.title}
           img={project.img}
           tech={project.tech}
         />
       ))}
     </div>
-    </div>
+  </div>
   )
 }
 
