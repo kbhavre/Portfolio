21d09bf4214daedafe09e5e11efd346d3d3f9df4
Ronit-gurjar <ronitgurjar999@gmail.com>
2025-01-18 11:52:20 +0530

    Fix: issue with asset import
    the issue is caused due to due to how Vite handles assets in the public directory. Vite's public directory is intended for static assets that will be served at the root path and directly accessed via URLs, rather than being imported in your JavaScript or TypeScript code.
    
    to fix it, simply use public urls, instead of importing,i.e.:-
    {
        img: "/DiaAlert.png", //like this
    }

--- src/constants/index.js
+++ src/constants/index.js
@@ -1,8 +1,3 @@
-import img1 from '../../public/NinjaNotes.png';
-import img2 from '../../public/Medicare.png';
-import img3 from '../../public/Nexcent.png';
-import img4 from '../../public/DiaAlert.png';
-
 export const lgNavLinks = [
     {   id: 1,
         title: "Work",
@@ -50,7 +45,7 @@ export const projects = [
     {
         id: 1,
         title: "Ninja Notes",
-        img: img1,
+        img: "/NinjaNotes.png",
         tech: ["React", "Aceternity", "TypeScript", "Node.js", "MongoDB"],
         para: "Ninja Notes is a platform designed for university students, offering handwritten notes, previous year papers, and technical blogs. It allows students to save their notes for future reference, enhancing their learning experience and helping them perform better academically.",
         linkref: "https://ninjanotesorg.vercel.app/"
@@ -58,7 +53,7 @@ export const projects = [
     {
         id: 2,
         title: "Medicare",
-        img: img2,
+        img: "/Medicare.png",
         tech: ["React", "Javascript", "Stripe.js", "Node.js", "MongoDB"],
         para: "Medicare is a healthcare platform built during a hackathon, focusing on streamlining the doctor appointment process. It allows users to easily book appointments with doctors, improving access to healthcare services and ensuring a smooth scheduling experience.",
         linkref: "https://doctor-appointment-booking-silk.vercel.app/" 
@@ -66,7 +61,7 @@ export const projects = [
     {
         id: 3,
         title: "Nexcent",
-        img: img3,
+        img: "/Nexcent.png",
         tech: ["React", "Tailwind", "Stripe.js", "Node.js", "MongoDB"],
         para: "DiaAlert is a web application developed during my machine learning internship. It predicts whether a user is diabetic based on inputs like BMI, age, and glucose level, providing accurate results. This project demonstrates practical application of ML in healthcare.",
         linkref: "https://nexcent-sigma-eight.vercel.app/",
@@ -74,7 +69,7 @@ export const projects = [
     {
         id: 4,
         title: "DiaAlert",
-        img: img4,
+        img: "/DiaAlert.png",
         tech: ["Python", "Numpy", "Pandas", "Matplotlib", "Streaml"],
         para: "DiaAlert is a web application developed during my machine learning internship. It predicts whether a user is diabetic based on inputs like BMI, age, and glucose level, providing accurate results. This project demonstrates practical application of ML in healthcare.",
         linkref: "https://dia-alert.streamlit.app/",
